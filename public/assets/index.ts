c501e68271f7aa8597b53fa9fff6950689e9af6e
hanbyeol kwon <janviertrois@naver.com>
2023-12-21 18:38:58 +0900

    Added icons

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,5 +1,7 @@
 import logo from "./images/logo-icon.png";
 import prompt from './images/prompt.png';
 import close from './images/close.png';
+import folder from './images/folder.png'
+import openFolder from './images/folder-open.png'
 
-export { logo, prompt, close }
+export { logo, prompt, close, folder, openFolder }
