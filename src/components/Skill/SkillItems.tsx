decbbb12e5c39b3c059e928522108b18ce4639da
hanbyeol kwon <janviertrois@naver.com>
2023-12-22 20:36:58 +0900

    Add config.ts

--- src/components/Skill/SkillItems.tsx
+++ src/components/Skill/SkillItems.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
+import { basicFetch } from "@/app/api/fetchFunction";
+import { skillUrl } from "../../../config";
 
-interface skillProps {
+interface Skill {
   id: number;
   title: string;
   img: string;
 }
 
 const SkillItems = async () => {
-  const skillsResponse = await fetch("https://api.jsonsilo.com/public/3cc4c28d-b100-44b5-99d7-406a3672c96c");
-
-  const skillsJson = await skillsResponse.json();
+  const skills = await basicFetch<Skill[]>(skillUrl);
 
   return (
     <div className="modal-content modal-skill">
-      {skillsJson.map((skill: skillProps) => (
+      {skills.map((skill: Skill) => (
         <div key={skill.id} className="folder">
           <Image src={skill.img} width={43} height={43} alt={skill.title} />
           {skill.title}
