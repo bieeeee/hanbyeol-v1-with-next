a2bbc7c852f3fc3c18b2608c0d9dd3b5d40a134a
hanbyeol kwon <janviertrois@naver.com>
2023-12-22 19:58:40 +0900

    Delete navbar, add projects route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
+import Link from 'next/link'
 import Terminal from '@/components/Terminal/Terminal'
 import styles from './page.module.css'
 import Navbar from '@/components/Navbar/Navbar'
 import Skill from '@/components/Skill/Skill'
 import SkillItems from '@/components/Skill/SkillItems'
+import Image from 'next/image'
+import { folder } from '@images'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Navbar />
       <div className={styles.container}>
         <Terminal />
-        <Skill>
-          <SkillItems />
-        </Skill>
+        <div className={styles.icons}>
+          <Skill>
+            <SkillItems />
+          </Skill>
+          <Link href="/projects" className='folder'>
+            <Image src={folder} width={48} height={48} alt='projects-folder' />
+            Projects
+          </Link>
+        </div>
       </div>
     </main>
   )
