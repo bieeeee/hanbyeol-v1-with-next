67db8b1869f6bacfc28db37b3dfb195d349fa363
hanbyeol kwon <janviertrois@naver.com>
2023-12-25 17:31:46 +0900

    feat: Remove project list, get all projects from server, change folder title to dynamic value, update onclick function with data index

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,102 +1,57 @@
 'use client'
-import { useState } from "react";
+import styles from './Project.module.scss';
 import Image from "next/image";
+import { useState } from "react";
 import { close, folder, openFolder } from "@images";
 
-type Project = {
-  id: string;
-  title: string;
-}
 
-type ProjectStates = {
-  [key: string]: boolean;
-}
+const Project = ({ projects }: { projects: Project[] }) => {
+  const [isSelected, setIsSelcted] = useState<boolean>(false);
+  const [selectedId, setSelectedId] = useState<number>();
 
-const projects: Project[] = [
-  { id: 'plan', title: 'Plan For You' },
-  { id: 'varcharforyou', title: 'Varchar For You' },
-  { id: 'v1', title: 'Hanbyeol.me' },
-  { id: 'zapfit', title: 'Zapfit.ca' },
-  { id: 'only', title: 'OnlyDevs' },
-  { id: 'booking', title: 'Booking.com' },
-];
-
-function Project({ data }: any) {
-  const [selectedId, setSelectedId] = useState("")
-  const [projectStates, setProjectStates] = useState(
-    projects.reduce((acc, project: Project) => {
-      acc[project.id] = false;
-      return acc;
-    }, {} as ProjectStates
-    ));
-
-  const toggleProjectModal = (e: any, id: string) => {
-    e.stopPropagation();
-    setProjectStates((prevStates: ProjectStates) => ({
-      ...prevStates,
-      [id]: !prevStates[id],
-    }));
+  const toggleProjectItem = (id: number) => {
+    setIsSelcted(!isSelected);
+    setSelectedId(id - 1);
   };
 
   return (
-    <main>
-      <div className="modal">
-        <div className="modal-container">
-          <div className="modal-bar">
-            <div className="modal-bar-left">
-              <Image src={openFolder} width={12} height={12} alt="folder-opened" priority />
-              <p>Projects</p>
-            </div>
-            <button
-              className='close-modal'
-            >
-              <Image src={close} width={12} height={12} alt="folder-opened" />
-            </button>
+    <>
+      <div className={`${styles.projectContainer} border`}>
+        <div className="modal-bar">
+          <div className="modal-bar-left">
+            <Image src={openFolder} width={12} height={12} alt="folder-opened" priority />
+            <p>
+              {isSelected && selectedId ?
+                projects[selectedId].title
+                :
+                "Projects"
+              }
+            </p>
           </div>
-          <div className="project-modal-content">
-            <div className="modal-content-grid">
-              {projects.map((project) => (
-                <div
-                  key={project.id}
-                  onClick={(e) => toggleProjectModal(e, project.id)}
-                  className='btn-modal project-modal-icon'
-                >
-                  <Image src={folder} width={48} height={48} alt="folder" />
-                  <p>{project.title}</p>
-                </div>
-              ))}
-            </div>
-          </div>
+          <button className="close-modal" onClick={() => toggleProjectItem(0)}>
+            <Image src={close} width={12} height={12} alt="folder-opened" />
+          </button>
         </div>
-      </div>
-
-
-      {projects.map((project) => (
-        <div key={project.id}>
-          {projectStates[project.id] && (
-            <div className='modal'>
-              <div className='modal-container'>
-                <div className="modal-bar">
-                  <div className="modal-bar-left">
-                    <Image src={openFolder} width={12} height={12} alt="folder-opened" priority />
-                    <p>{project.title}</p>
-                  </div>
-                  <button
-                    onClick={(e) => toggleProjectModal(e, project.id)}
-                    className='close-modal'
-                  >
-                    <Image src={close} width={12} height={12} alt="folder-opened" />
-                  </button>
-                </div>
-                <div className="project-modal-content">
-                  {data[selectedId].desc}
-                </div>
+        <div className={`${styles.projectContent} clickedBorder`}>
+          {!isSelected ? (
+            projects.map((project: Project) => (
+              <div
+                key={project.id}
+                onClick={() => toggleProjectItem(project.id)}
+                className='folder'
+              >
+                <Image src={folder} width={48} height={48} alt="folder" />
+                <p>{project.title}</p>
               </div>
+            ))
+          ) : (
+            <div className="project-modal-content">
+              {selectedId !== undefined && projects[selectedId].desc}
             </div>
           )}
         </div>
-      ))}
-    </main>
+      </div>
+    </>
   )
 }
 
