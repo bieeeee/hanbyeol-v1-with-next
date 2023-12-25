baff85632ae986d3956ce755e12f1d49feb0b5ee
hanbyeol kwon <janviertrois@naver.com>
2023-12-25 16:29:08 +0900

    fix: Update project url and provide access key

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,14 @@
-import styles from '../page.module.css';
+import styles from '../page.module.scss';
 import Project from '@/components/Project/Project';
 import { basicFetch } from '../api/fetchFunction';
-import { projectUrl } from '../../../config';
-
-type ProjectItem = {
-  [projectId: string]: {
-    desc: string
-  }
-}
+import { projectKey, projectUrl } from '../../../config';
 
 export default async function Home() {
-  const projectItems = await basicFetch<ProjectItem[]>(projectUrl);
+  const projectItems = await basicFetch<Project[]>(projectUrl, projectKey);
 
   return (
-    <main className={styles.main}>
-        <Project data={projectItems} />
+    <main className={styles.projectMain}>
+      <Project data={projectItems} />
     </main>
   )
 }
