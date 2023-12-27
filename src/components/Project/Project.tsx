c64da443e54e0d5605e48931c7a0d9de163120b3
hanbyeol kwon <janviertrois@naver.com>
2023-12-27 12:41:45 +0900

    feat: Disable button when there isn't a selectedId

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -3,11 +3,16 @@ import styles from './Project.module.scss';
 import Image from "next/image";
 import { useState } from "react";
 import { close, folder, openFolder } from "@images";
+import { BsFillPeopleFill } from "@react-icons/all-files/bs/BsFillPeopleFill";
+import { FaRegCalendarAlt } from "@react-icons/all-files/fa/FaRegCalendarAlt";
+import { FaCode } from "@react-icons/all-files/fa/FaCode";
 
 
 const Project = ({ projects }: { projects: Project[] }) => {
   const [selectedId, setSelectedId] = useState<number>(-1);
   const [isSelected, setIsSelcted] = useState<boolean>(false);
+  const selectedProject = projects[selectedId];
+  const image = selectedProject?.image;
 
   const toggleProjectItem = (id: number) => {
     setSelectedId(id - 1);
@@ -17,8 +22,8 @@ const Project = ({ projects }: { projects: Project[] }) => {
   return (
     <>
       <div className={`${styles.projectContainer} border`}>
-        <div className="modal-bar">
-          <div className="modal-bar-left">
+        <div className="modalBar">
+          <div className="modalBarLeft">
             <Image src={openFolder} width={12} height={12} alt="folder-opened" priority />
             <p>
               {isSelected ?
@@ -28,25 +33,48 @@ const Project = ({ projects }: { projects: Project[] }) => {
               }
             </p>
           </div>
-          <button className="close-modal" onClick={() => toggleProjectItem(0)}>
+          <button className="close-modal" disabled={!isSelected} onClick={() => toggleProjectItem(0)}>
             <Image src={close} width={12} height={12} alt="folder-opened" />
           </button>
         </div>
-        <div className={`${styles.projectContent} clickedBorder`}>
+        <div className='modal-content'>
           {!isSelected ? (
-            projects.map((project: Project) => (
-              <div
-                key={project.id}
-                onClick={() => toggleProjectItem(project.id)}
-                className='folder'
-              >
-                <Image src={folder} width={48} height={48} alt="folder" />
-                <p>{project.title}</p>
-              </div>
-            ))
+            <div className={styles.projectList}>
+              {projects.map((project: Project) => (
+                <div
+                  key={project.id}
+                  onClick={() => toggleProjectItem(project.id)}
+                  className='folder'
+                >
+                  <Image src={folder} width={48} height={48} alt="folder" />
+                  <p>{project.title}</p>
+                </div>
+              ))}
+            </div>
           ) : (
-            <div className="project-modal-content">
-              {selectedId !== undefined && projects[selectedId].desc}
+            <div className={styles.projectContent}>
+              <div className={styles.projectInfo}>
+                <div className={styles.projectImg}>
+                  <Image src={image} fill style={{ objectFit: "cover" }} alt={selectedProject.title} />
+                </div>
+                <div className={styles.projectDetail}>
+                  <h1>{selectedProject.title}</h1>
+                  <div>
+                    <div>
+                      <BsFillPeopleFill size={13} />
+                      <p>1</p>
+                    </div>
+                    <div>
+                      <FaRegCalendarAlt size={13} />
+                      <p>2023/04/29 - 2023/11/20</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.projectDetail}>
+                {selectedProject.desc}
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum cupiditate repellat ab. At, ipsum mollitia perspiciatis tempore fuga fugit minus libero, doloremque exercitationem est asperiores esse accusantium, quod deserunt et!
+              </div>
             </div>
           )}
         </div>
