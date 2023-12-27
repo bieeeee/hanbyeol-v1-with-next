456afa13c35bed43a4ce5862848236e826eb525d
hanbyeol kwon <janviertrois@naver.com>
2023-12-27 12:47:07 +0900

    fix: Rename the function

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,7 @@ import styles from '../page.module.scss';
 import Project from '@/components/Project/Project';
 import { basicFetch } from '../api/fetchFunction';
 
-export default async function Home() {
+export default async function Projects() {
   const projectKey = {"X-MASTER-KEY": process.env.PROJECT_KEY,"X-Bin-Meta": false}
   const projects = await basicFetch<Project[]>(process.env.PROJECT_URL, projectKey);
 
