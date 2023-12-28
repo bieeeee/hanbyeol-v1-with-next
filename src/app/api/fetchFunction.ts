743b821a6631506fbdeb246cb404520e122d9d6a
hanbyeol kwon <janviertrois@naver.com>
2023-12-28 12:40:52 +0900

    fix: Handle enpoint undefined issue

--- src/app/api/fetchFunction.ts
+++ src/app/api/fetchFunction.ts
@@ -1,6 +1,9 @@
 export const basicFetch = async <returnType>(endpoint: string, customHeaders?: Record<string, string|boolean>): Promise<returnType> => {
   const defaultHeaders = {};
   const headers = customHeaders ? {...defaultHeaders, ...customHeaders} : defaultHeaders;
+
+  if (endpoint === undefined) throw new Error("Endpoint error")
+
   const response = await fetch(endpoint, {
     headers: headers
   });
