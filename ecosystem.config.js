a8b3484d1a7a7aa850b7083497b71fa2b376d297
hanbyeol kwon <janviertrois@naver.com>
2023-12-28 13:43:03 +0900

    chore: Edit env setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   apps: [{
     script: 'npm start',
+    env: {
+      env_production: "production"
+    }
   }],
   deploy: {
     production: {
@@ -11,7 +14,7 @@ module.exports = {
       repo: 'git@github.com:bieeeee/hanbyeol-v1-with-next.git',
       path: '/home/ubuntu',
       'pre-deploy-local': '',
-      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env',
+      'post-deploy': 'source ~/.nvm/nvm.sh &&  && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
