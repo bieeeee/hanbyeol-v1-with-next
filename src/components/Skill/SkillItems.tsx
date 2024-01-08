1e69c63db86bff7df97b4085fc7f8f960aed9436
hanbyeol kwon <janviertrois@naver.com>
2024-01-08 19:37:46 +0900

    feat: Add language dataset

--- src/components/Skill/SkillItems.tsx
+++ src/components/Skill/SkillItems.tsx
@@ -8,15 +8,27 @@ const SkillItems = async () => {
     cur.stack.forEach((skill) => acc[skill] = (acc[skill] || 0) + 1)
     return acc
   }, {});
-
   const sortedSkills = Object.entries(skills).sort((a, b) => b[1] - a[1])
   const skillData = Object.fromEntries(sortedSkills)
 
+  const repos = await basicFetch<any>("https://api.github.com/users/bieeeee/repos")
+
+  const languages = repos.reduce((acc: Skills, cur: any) => {
+    acc[cur.language] = (acc[cur.language] || 0) + 1
+    return acc
+  }, {})
+
   const data = {
-    labels: Object.keys(skillData),
+    labels: [...Object.keys(skillData), ...Object.keys(languages)],
     datasets: [
       {
+        labels: Object.keys(skillData),
         data: Object.values(skillData),
+        hoverOffset: 4,
+      },
+      {
+        labels: Object.keys(languages),
+        data: Object.values(languages),
         hoverOffset: 4
       }
     ]
