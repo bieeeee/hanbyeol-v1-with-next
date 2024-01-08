6cde21aea36bc4f3e112ec50dc41abc691c8a2a9
hanbyeol kwon <janviertrois@naver.com>
2024-01-08 19:39:17 +0900

    feat: Display label inside chart, update dataset labels and tooltip titles

--- src/components/Skill/Skill.tsx
+++ src/components/Skill/Skill.tsx
@@ -5,8 +5,38 @@ import { close, folder, openFolder } from '@images';
 import { useState } from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, Colors } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-ChartJS.register(ArcElement, Tooltip, Legend, Colors);
+ChartJS.register(ArcElement, ChartDataLabels, Tooltip, Legend, Colors);
+
+const options = {
+  plugins: {
+    legend: {
+      display: false
+    },
+    datalabels: {
+      align: "center",
+      formatter: function (_: any, context: any) {
+        return context.dataset.labels[
+          context.dataIndex
+        ]
+      },
+      color: "#fff",
+      font: {
+        weight: 600,
+        size: 12
+      }
+    },
+    tooltip: {
+      callbacks: {
+        title: function(context) {
+          return context[0].dataset.labels[context[0].dataIndex];
+        }
+      }
+    }
+  }
+}
+
 
 const Skill = ({ data }: any) => {
   const [skillModal, setSkillModal] = useState(false);
@@ -28,14 +58,16 @@ const Skill = ({ data }: any) => {
               <div className="modalBar">
                 <div className="modalBarLeft">
                   <Image src={openFolder} alt='open-folder-icon' />
-                  <p>Skills</p>
+                  <p>프로젝트에 사용한 기술빈도</p>
                 </div>
                 <div onClick={toggleModal} className='close-modal'>
                   <Image src={close} width={12} height={12} alt='close-button' />
                 </div>
               </div>
               <div className="modal-content">
-                <Doughnut data={data} />
+                <div className={styles.chart}>
+                  <Doughnut data={data} plugins={[ChartDataLabels]} options={options} />
+                </div>
               </div>
             </div>
           </div>
