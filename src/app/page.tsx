5a7d32a329f1c6994fc37a3c3e52ae68b10d8393
hanbyeol kwon <janviertrois@naver.com>
2024-01-08 17:15:31 +0900

    refactor: Remove flex box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,21 +9,19 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Terminal />
-      <div className={styles.icons}>
-        <SkillItems />
-        <Link href="/projects" className='folder'>
-          <Image src={folder} width={48} height={48} alt='projects-folder' />
-          <p>Projects</p>
-        </Link>
-        <a href="https://www.linkedin.com/in/hanbyeol-kwon/" target='_blank' rel="noreferrer noopener" className='folder' style={{ textDecoration: 'none', color: 'black' }}>
-          <Image src={linkedin} height={48} width={48} alt='linkedin' priority />
-          <p>LinkedIn</p>
-        </a>
-        <a href="https://github.com/bieeeee" target='_blank' rel="noreferrer noopener" className='folder' style={{ textDecoration: 'none', color: 'black' }}>
-          <Image src={github} height={48} width={48} alt='github' priority />
-          <p>Github</p>
-        </a>
-      </div>
+      <SkillItems />
+      <Link href="/projects" className='folder'>
+        <Image src={folder} width={48} height={48} alt='projects-folder' />
+        <p>Projects</p>
+      </Link>
+      <a href="https://www.linkedin.com/in/hanbyeol-kwon/" target='_blank' rel="noreferrer noopener" className='folder' style={{ textDecoration: 'none', color: 'black' }}>
+        <Image src={linkedin} height={48} width={48} alt='linkedin' priority />
+        <p>LinkedIn</p>
+      </a>
+      <a href="https://github.com/bieeeee" target='_blank' rel="noreferrer noopener" className='folder' style={{ textDecoration: 'none', color: 'black' }}>
+        <Image src={github} height={48} width={48} alt='github' priority />
+        <p>Github</p>
+      </a>
     </main>
   )
 }
