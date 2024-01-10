fcb4e9d9a1049290fd293eb035b5154360fb7813
hanbyeol kwon <janviertrois@naver.com>
2024-01-11 08:00:10 +0900

    Add calculator icon

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -5,5 +5,6 @@ import folder from './images/folder.png';
 import openFolder from './images/folder-open.png';
 import linkedin from './images/linkedin.png';
 import github from './images/github.png';
+import calculator from './images/calculator.png';
 
-export { logo, prompt, close, folder, openFolder, linkedin, github }
+export { logo, prompt, close, folder, openFolder, linkedin, github, calculator }
