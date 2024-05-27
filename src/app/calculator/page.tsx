bf7937eb57ed02276504ad5aa4c19d5506aa58a8
bieeeee <janviertrois@naver.com>
2024-05-27 17:12:05 +0900

    [refactor] Replace module.scss with Tailwindcss

---
+++ src/app/calculator/page.tsx
@@ -0,0 +1,11 @@
+import Calculator from '@/components/Calculator/Calculator';
+
+const page = () => {
+  return (
+    <main className={`w-full h-full flex items-center justify-center p-5`}>
+      <Calculator />
+    </main>
+  )
+}
+
+export default page
