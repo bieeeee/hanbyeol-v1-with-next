765702638eff154b14845e24c360d3a02e0bc3c9
bieeeee <janviertrois@naver.com>
2024-05-27 16:26:15 +0900

    [refactor] Replace div with Icon component and update image path

---
+++ src/app/page.tsx
@@ -0,0 +1,63 @@
+import Terminal from '@/components/Terminal/Terminal';
+import { folder, linkedin, github, calculator, velog } from '@assets/images';
+import { Icon } from '@/components/ui/icon';
+
+export default function Home() {
+  return (
+    <div className={"main"}>
+      <Terminal />
+      {icons.map((icon, i) => (
+        <Icon
+          key={i}
+          title={icon.title}
+          alt={icon.alt}
+          src={icon.src}
+          href={icon.href}
+          target={icon.target}
+          rel={icon.rel}
+        />
+      ))}
+    </div>
+  )
+}
+
+const target = '_blank';
+const rel = 'noreferrer noopener';
+export const icons = [
+  {
+    title: 'Projects',
+    alt: 'projects-folder',
+    src: folder,
+    href: '/projects'
+  },
+  {
+    title: 'N빵 계산기',
+    alt: 'calculator',
+    src: calculator,
+    href: '/calculator'
+  },
+  {
+    title: 'Velog',
+    alt: 'velog',
+    src: velog,
+    href: 'https://velog.io/@bieeeee',
+    target: target,
+    rel: rel
+  },
+  {
+    title: 'LinkedIn',
+    alt: 'linkedin',
+    src: linkedin,
+    href: 'https://www.linkedin.com/in/hanbyeol-kwon/',
+    target: target,
+    rel: rel
+  },
+  {
+    title: 'Github',
+    alt: 'github',
+    src: github,
+    href: 'https://github.com/bieeeee',
+    target: target,
+    rel: rel
+  },
+]
