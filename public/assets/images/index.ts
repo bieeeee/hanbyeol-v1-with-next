322ffe985e34ba8f32bf5ba4492ddc1377d71aa7
bieeeee <janviertrois@naver.com>
2024-05-27 16:24:50 +0900

    [feat] Add index page for images

---
+++ public/assets/images/index.ts
@@ -0,0 +1,11 @@
+import logo from "./logo-icon.png";
+import prompt from './prompt.png';
+import close from './close.png';
+import folder from './folder.png';
+import openFolder from './folder-open.png';
+import linkedin from './linkedin.png';
+import github from './github.png';
+import calculator from './calculator.png';
+import velog from './velog.png';
+
+export { logo, prompt, close, folder, openFolder, linkedin, github, calculator, velog }
